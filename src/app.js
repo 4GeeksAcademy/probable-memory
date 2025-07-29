e98341e106eeea6f4ee944ace5a76a33e21f7d12
Britney Escoffery <britneybescoffery@gmail.com>
2025-07-29 01:00:14 +0000

    added function to generate random card and symbol

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const div = document.querySelector("#card");
+const button = document.querySelector("#generateBtn");
+
+const myArray = ["♠", "♥", "♦", "♣"];
+const myNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+console.log("myNumbers",myNumbers.length);
+
+function getRandomCard(array) {
+  const index = Math.floor(Math.random () * array.length);
+  return array[index];
+}
+
+function generateRandomCard (){
+  const randomNumber = getRandomCard(myNumbers);
+  const randomSymbol = getRandomCard(myArray);
+  const results = `${randomSymbol}${randomNumber}`
+  console.log("results",results)
+  return results;
+
+}
+window.generateRandomCard = generateRandomCard;
+
+document.querySelector("#generateBtn").addEventListener("click", ()=> {
+const card = generateRandomCard();
+document.querySelector("#card").textContent = card;
+
+});
+
+const randomDec = Math.random()
+// //0.654567654
+const randomLongNum = randomDec * myArray.length
+// // long dec numbers from 0-5
+console.log("randomLongNum",randomLongNum)
+const randomNumNoDec = Math.floor(randomLongNum)
+console.log("randomNumNoDec",randomNumNoDec)
+
+// // removed decimals
+
+div.innerHTML = myArray[randomNumNoDec]
+
 };
+
